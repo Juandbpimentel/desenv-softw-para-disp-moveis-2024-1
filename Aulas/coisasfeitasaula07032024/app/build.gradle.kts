386325c6bc13ffb2041f57d8c11736a2ccca951c
Juan Pimentel <juandbpimentel@alu.ufc.br>
2024-03-08 00:31:40 -0300

    Tentando settar as coisas no repo

--- Aulas/coisasfeitasaula07032024/app/build.gradle.kts
+++ Aulas/coisasfeitasaula07032024/app/build.gradle.kts
@@ -15,6 +15,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    dataBinding {
+        enable = true
+    }
 
     buildTypes {
         release {
@@ -35,7 +38,7 @@ android {
 }
 
 dependencies {
-
+    annotationProcessor("com.android.databinding:compiler:3.5.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
